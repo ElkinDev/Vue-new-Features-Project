9b840da73f28f843211aa842c85f48b50e372e85
ElkinDev <elkinmendoza00@gmail.com>
2020-04-09 19:55:05 +1000

    nesting components examples

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,39 @@
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
         <v-row>
-      <v-col cols="3">
+      <v-col cols="2">
       <menudrawer app>
     
     </menudrawer>
 
       </v-col>
 
-      <v-col cols="9" md="8">
-  Duis ut nostrud aliquip pariatur commodo et.Enim ullamco eiusmod ipsum ad.Laborum reprehenderit sunt irure ad quis dolore cupidatat in sint magna occaecat commodo enim.
+      <v-col cols="9" md="9">
+      <v-container class="grey lighten-5 text-center ">
+          <v-row >
+      <v-col
+        v-for="(card, index) in cards"
+        :key="card"
+        cols="12"
+        sm="4"
+        @click="card.showdetail=!card.showdetail" 
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+          
+        >
+          {{card.title}} - {{index+1}}
+         <h4 v-show="card.showdetail">
+          {{card.text}}
+         
+         </h4>
+        </v-card>
+      </v-col>
+    </v-row>
+ 
+      </v-container>
 
       </v-col>
         
@@ -52,7 +76,17 @@
     },
 
     data: () => ({
-      //
+      cards:[
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+        {title:'Card',text:'Laboris adipisicing exercitation ut irure ea tempor.',showdetail:false},
+      ]
     }),
   };
   </script>
